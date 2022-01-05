6009b136679a5a99e8dc32e1b89d56add93843e3
Naysa Bots <96517910+Naysabots@users.noreply.github.com>
2022-01-05 11:09:28 +0530

    Update cb_buttons.py

--- plugins/cb_buttons.py
+++ plugins/cb_buttons.py
@@ -7,7 +7,7 @@ import os
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
 else:
-    from config import Config
+    from sample_config import Config
 
 from plugins.youtube_dl_button import youtube_dl_call_back
 from plugins.dl_button import ddl_call_back
