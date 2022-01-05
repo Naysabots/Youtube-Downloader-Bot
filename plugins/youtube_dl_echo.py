a2a5f5d87ec08a562c84af30f41b858cf49b11b2
Naysa Bots <96517910+Naysabots@users.noreply.github.com>
2022-01-05 11:07:57 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -22,7 +22,7 @@ import requests
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
 else:
-    from config import Config
+    from sample_config import Config
 
 from translation import Translation
 
